7136b81cef55baa0e7390d1e348e0e13a9678d39
zxw <you@example.com>
2019-08-01 10:53:56 +0800

    自定义插件

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,6 +9,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+const HelloWorldPlugin = require('./Plugins/helloWorld')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
@@ -64,7 +65,11 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         to: config.dev.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    // 自定义插件
+    new HelloWorldPlugin({
+      options: true
+    })
   ]
 })
 
